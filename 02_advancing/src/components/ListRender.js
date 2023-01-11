2b5faa9c34bdf178194432734988ec92b37ea8ca
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-11 11:45:03 +0044

    Previews state

--- 02_advancing/src/components/ListRender.js
+++ 02_advancing/src/components/ListRender.js
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 
 const ListRender = () => {
-    const [users] = useState([
+
+    const [users, setUsers] = useState([
         { id: 1, name: "Juliana", age: 27},
         { id: 2, name: "Karla", age: 27},
+        { id: 3, name: "Ceci", age: 4},
+        { id: 4, name: "Rafa", age: 10},
+        { id: 5, name: "Totobibi", age: 1},
     ])
 
+    const randomDelete = () => {
+        const randomNum = Math.ceil(Math.random() * (users.length + 1))
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNum !== user.id) 
+        })
+    }
+
     return (
-        <div>
+        <div className='listData'>
             <ul>
                 {users.map((user) => (
                     <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
+
+            <button onClick={randomDelete}>Delete random user</button>
         </div>
     )
 }
