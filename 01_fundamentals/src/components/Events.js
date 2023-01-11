ae5341423320c95febee672cd2224cb0f390f4fe
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-11 09:35:01 +0044

    Inline event

--- 01_fundamentals/src/components/Events.js
+++ 01_fundamentals/src/components/Events.js
@@ -1,15 +1,18 @@
 const Events = () => {
 
     const handleMyEvent = (e) => {
-        console.log(e)
-
         console.log('Clicked')
     }
 
     return (
         <div>
+
             <div>
                 <button onClick={handleMyEvent}>Click</button>
+            </div>
+
+            <div>
+                <button onClick={() => console.log('Inline event')}>Different Click</button>
             </div>
         </div>
     )
