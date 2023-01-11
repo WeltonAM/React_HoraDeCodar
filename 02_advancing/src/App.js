18c73f411b2704c15571115e2b2e69cd3fe481e0
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-11 10:29:18 +0044

    Advancing Photo as a variant

---
+++ 02_advancing/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import CountryPhoto from './assets/img/pic1.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Advancing</h2>
+      <div>
+        <img src={CountryPhoto}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
