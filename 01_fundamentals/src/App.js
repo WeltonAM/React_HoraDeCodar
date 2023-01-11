dc1c9dbddbfb57a58c9777279c80c0318ef10a59
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-11 09:17:21 +0044

    Template expressions

--- 01_fundamentals/src/App.js
+++ 01_fundamentals/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import FirstComponent from './components/FirstComponent';
+import TemplateExpressions from './components/TemplateExpressions'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, Juliana</h1>
       <FirstComponent />
+      <TemplateExpressions />
     </div>
   );
 }
---
+++ 01_fundamentals/src/components/TemplateExpressions.js
@@ -0,0 +1,18 @@
+const TemplateExpressions = () => {
+    
+    const name = 'Juliana'
+
+    const data = {
+        age: 31,
+        job: "Programmer"
+    }
+    
+    return (
+        <div>
+            <p>Hello, {name}!</p>
+            <p>You are a {data.job}</p>
+        </div>
+    )
+}
+
+export default TemplateExpressions
