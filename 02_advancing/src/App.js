13b8cda6597fb5d87a0dee0c71ac2e14e6709b15
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-12 08:47:48 +0044

    Executing function from parent

--- 02_advancing/src/App.js
+++ 02_advancing/src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 import './App.css';
 
 import CountryPhoto from './assets/img/pic1.jpg'
 import CarDetails from './components/CarDetails';
 import ConditionalRender from './components/ConditionalRender';
 import Container from './components/Container';
+import ExecutingFunc from './components/ExecutingFunc';
 import Fragment from './components/Fragment';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
@@ -12,11 +15,21 @@ import ShowUserName from './components/ShowUserName';
 function App() {
 
   const cars = [
-    {id: 1, brand: "Ford", color: "Yellow", km: 10, newCar: false },
-    {id: 2, brand: "VW", color: "Blue", km: 110, newCar: false },
-    {id: 3, brand: "Chav", color: "Black", km: 11230, newCar: false },
-    {id: 4, brand: "Renault", color: "Red", km: 0, newCar: true },
+    { id: 1, brand: "Ford", color: "Yellow", km: 10, newCar: false },
+    { id: 2, brand: "VW", color: "Blue", km: 110, newCar: false },
+    { id: 3, brand: "Chav", color: "Black", km: 11230, newCar: false },
+    { id: 4, brand: "Renault", color: "Red", km: 0, newCar: true },
   ]
+
+  function showMessage() {
+    alert('Father event')
+  }
+
+  const [message, setMessage] = useState("")
+
+  const handleMgs = (msg) => {
+    setMessage(msg)
+  }
 
   return (
     <div className="App">
@@ -24,7 +37,7 @@ function App() {
       <h2>Advancing</h2>
 
       <div>
-        <img src={CountryPhoto}/>
+        <img src={CountryPhoto} />
       </div>
 
       <ManageData />
@@ -38,9 +51,9 @@ function App() {
       <CarDetails brand="Chav" km={100.000} color="Blue" newCar={false} />
 
       <CarDetails brand="Ford" km={0} color="Blue" newCar={true} />
-      
+
       {cars.map((car) => (
-        <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+        <CarDetails key={car.id} brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
       ))}
 
       <Fragment />
@@ -48,6 +61,12 @@ function App() {
       <Container>
         <p>Child component</p>
       </Container>
+
+      
+
+      <ExecutingFunc myFunction={showMessage} />
+
+      {/* State lift */}
 
     </div>
   );
---
+++ 02_advancing/src/components/ExecutingFunc.js
@@ -0,0 +1,10 @@
+const ExecutingFunc = ({myFunction}) => {
+  return (
+    <div>
+        <hr />
+        <button onClick={myFunction}>Exec Func</button>
+    </div>
+  )
+}
+
+export default ExecutingFunc
