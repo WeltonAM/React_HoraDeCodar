3b5d641c25d63dcbe84dc51f76dcb38d29a9fb39
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-12 09:58:43 +0044

    Comp css

--- 03_styling/src/App.js
+++ 03_styling/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
       
+      {/* CSS global */}
       <h1>CSS in React</h1>
+
+      {/* CSS Component */}
+      <MyComponent />
 
     </div>
   );
---
+++ 03_styling/src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+        <hr/>
+        <h3>Css Component</h3>
+
+        <p className="comp-parag">
+            "qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem".
+        </p>
+    </div>
+  )
+}
+
+export default MyComponent
