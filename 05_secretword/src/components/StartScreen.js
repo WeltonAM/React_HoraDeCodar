a39452c127221f5de20656e436225cbff19e09c6
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-13 07:51:50 +0044

    Config init and components

---
+++ 05_secretword/src/components/StartScreen.js
@@ -0,0 +1,12 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Palavra Secreta</h1>
+        <button >Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
