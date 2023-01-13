2175e084b1c12dd0b3d92c682a6f27069c7e65e7
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-13 08:50:13 +0044

    Game screen style

--- 05_secretword/src/components/Game.js
+++ 05_secretword/src/components/Game.js
@@ -1,12 +1,38 @@
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
-  return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finalizar Jogo</button>
-    </div>
-  )
+const Game = ({ verifyLetter }) => {
+    return (
+        <div className='game'>
+
+            <p className='points'>
+                <span>Pontuação: 000</span>
+            </p>
+
+            <h1>Qual é a palavra?</h1>
+
+            <h3 className='tip'>
+                Dica: <span>Dica...</span>
+            </h3>
+
+            <div className='wordContainer'>
+                <span className='letter'>A</span>
+                <span className='blankSquare'></span>
+            </div>
+
+            <div className='letterContainer'>
+                <p>Tente descobrir uma letra da palavra:</p>
+                <input type="text" name="letter" maxLength="1" required />
+                <button type="">Jogar</button>
+            </div>
+
+            <div className='wrongLettersContainer'>
+                <p>Letras já utilizadas:</p>
+                <span>a,</span>
+                <span>b,</span>
+            </div>
+
+        </div>
+    )
 }
 
 export default Game
