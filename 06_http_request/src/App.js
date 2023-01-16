c660140ec5bc5d551dbf4253de2c9824695536b7
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-16 10:56:19 +0044

    Post request

--- 06_http_request/src/App.js
+++ 06_http_request/src/App.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react'
 function App() {
 
   const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
   
   const url = "http://localhost:3000/products"
 
@@ -22,15 +24,59 @@ function App() {
     fetchData()
   }, [])
 
+  // Posting data
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+
+    // Dynamic loading
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    setName("")
+    setPrice("")
+
+  }
+
   return (
     <div className="App">
       <h1>Products list</h1>
 
       <ul style={{listStyle: 'none', padding: '0'}}>
         {products.map((product) => (
-          <li key={product.id}>{product.name} - R$:{product.price}</li>
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+
+      <div className='add-product'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label>
+            Price:
+            <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+
+          <input type="submit" value="Create" />
+        </form>
+      </div>
+
     </div>
   );
 }
