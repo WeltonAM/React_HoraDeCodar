a9face5e139c1c073429575423765252227a9a45
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-16 10:33:34 +0044

    Getting data from API

--- 06_http_request/src/App.js
+++ 06_http_request/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  const [products, setProducts] = useState([])
+  
+  const url = "http://localhost:3000/products"
+
+  // Getting data
+  useEffect(() => {
+    async function fetchData(){
+      const res = await fetch(url)
+  
+      const data = await res.json()
+  
+      setProducts(data)
+
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <h1>Products list</h1>
+
+      <ul style={{listStyle: 'none', padding: '0'}}>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$:{product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
