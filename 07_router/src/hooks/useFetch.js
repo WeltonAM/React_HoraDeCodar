1c07879524a028aded35f4e19d80add27ad7f707
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-17 09:30:55 +0044

    Loading data

---
+++ 07_router/src/hooks/useFetch.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react'
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+
+    // refactoring POST
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch, setCallFetch] = useState(null)
+
+    // loading
+    const [loading, setLoading] = useState(false)
+
+    // treating errors
+    const [errors, setErrors] = useState(null)
+
+    // getting id
+    const [itemId, setItemId] = useState(null)
+
+    const httpConfig = (data, method) => {
+        if (method === 'POST') {
+            setConfig({
+                method,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+
+            setMethod(method)
+        } else if (method === 'DELETE') {
+            setConfig({
+                method,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+
+            setMethod(method)
+            setItemId(data)
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            setLoading(true)
+
+            try {
+
+                const res = await fetch(url)
+
+                const json = await res.json()
+
+                setData(json)
+            } catch (error) {
+                setErrors("Something went wrong!")
+            }
+
+            setLoading(false)
+        }
+
+        fetchData()
+
+    }, [url, callFetch])
+
+    useEffect(() => {
+        const httpRequest = async () => {
+            
+            let json
+
+            if (method === 'POST') {
+                let fetchOptions = [url, config]
+
+                const res = await fetch(...fetchOptions)
+
+                const json = await res.json()
+
+                setCallFetch(json)
+            } else if (method === 'DELETE') {
+                const deleteUrl = `${url}/${itemId}`
+
+                const res = await fetch(deleteUrl, config)
+
+                json = await res.json()
+            }
+
+            setCallFetch(json)
+        }
+
+        httpRequest()
+    }, [config, method, url])
+
+    return { data, httpConfig, loading, errors };
+}
--- 07_router/src/pages/Home.js
+++ 07_router/src/pages/Home.js
@@ -1,6 +1,30 @@
+import { Link } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
+import './Home.css'
+
 const Home = () => {
+
+  const url = 'http://localhost:3000/products'
+  const { data: items, loading, errors } = useFetch(url)
+
   return (
-    <div>Home</div>
+    <div>
+     
+      <h2>Products</h2>
+
+      {errors && <p>{errors}</p>}
+
+      <ul className="products">
+        {items && items.map((item) => (
+          <li key={item.id}>
+            <h3>{item.name}</h3>
+            <p>$: {item.price}</p>
+          </li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
