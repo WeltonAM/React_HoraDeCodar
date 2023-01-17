f8d2cb6e3837be7119cd871bce2c4777a07cd973
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-17 10:21:15 +0044

    404

---
+++ 07_router/src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1>404</h1>
+        <h1>Page not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
