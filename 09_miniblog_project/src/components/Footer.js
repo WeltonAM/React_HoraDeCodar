ad5a1aff4e6d3125286f0260cc8fe98eecf50610
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-01-18 11:40:22 +0044

    Global styling

--- 09_miniblog_project/src/components/Footer.js
+++ 09_miniblog_project/src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     <footer className={styles.footer}>
         <h3>Write about you like most!</h3>
         <p>MiniBlog &copy; {new Date().getFullYear()}</p>
+        <p>By <a href="https://github.com/WeltonAM" target="_blank">WeltonMatosDev</a></p>
     </footer>
   )
 }
