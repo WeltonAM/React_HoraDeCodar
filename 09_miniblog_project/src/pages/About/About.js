dd8b59b2135d3b02c3e864491805bf954b6bf3b5
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-23 14:06:06 -0300

    Blocking pages

--- 09_miniblog_project/src/pages/About/About.js
+++ 09_miniblog_project/src/pages/About/About.js
@@ -1,8 +1,14 @@
-import './About.module.css'
+import { Link } from 'react-router-dom'
+import styles from './About.module.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h2>About the Mini<span>Blog</span></h2>
+      <p>This project is a React and Firebase made blog</p>
+
+      <Link to="/posts/create" className='btn'>Create Post</Link>
+    </div>
   )
 }
 
