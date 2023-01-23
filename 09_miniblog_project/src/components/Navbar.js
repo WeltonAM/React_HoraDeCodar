62e8ead060612f0bfbe1de3fd02698cf21a3dbcd
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-23 13:55:55 -0300

    Auth links

--- 09_miniblog_project/src/components/Navbar.js
+++ 09_miniblog_project/src/components/Navbar.js
@@ -1,33 +1,55 @@
 import styles from './Navbar.module.css'
 import { NavLink } from 'react-router-dom'
 
+import { useAuthentication } from '../hooks/useAuthentication'
+import { useAuthValue } from '../context/AuthContext'
+
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-        <NavLink className={styles.brand} to="/">
-            Mini<span>Blog</span>
-        </NavLink>
 
-        <ul className={styles.links_list}>
-            <li>
-                <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
-            </li>
+    const { user } = useAuthValue()
 
-            <li>
-                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Login</NavLink>
-            </li>
+    return (
+        <nav className={styles.navbar}>
+            <NavLink className={styles.brand} to="/">
+                Mini<span>Blog</span>
+            </NavLink>
 
-            <li>
-                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Register</NavLink>
-            </li>
-            
-            <li>
-                <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>About</NavLink>
-            </li>
-        </ul>
+            <ul className={styles.links_list}>
+                <li>
+                    <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : '')}>Home</NavLink>
+                </li>
 
-    </nav>
-  )
+                {!user && (
+                    <>
+                        <li>
+                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : '')}>Login</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : '')}>Register</NavLink>
+                        </li>
+                    </>
+                )}
+
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : '')}>New post</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : '')}>Dashboard</NavLink>
+                        </li>
+                    </>
+                )}
+
+                <li>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : '')}>About</NavLink>
+                </li>
+            </ul>
+
+        </nav>
+    )
 }
 
 export default Navbar
---
+++ 09_miniblog_project/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import styles from './Dashboard.module.css'
+
+const dashboard = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default dashboard
