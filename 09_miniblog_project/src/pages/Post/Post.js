f8d24e349550fda06fb560768dd454450706f603
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-24 16:38:42 -0300

    Post view, hook

--- 09_miniblog_project/src/pages/Post/Post.js
+++ 09_miniblog_project/src/pages/Post/Post.js
@@ -1,15 +1,36 @@
 import styles from './Post.module.css'
 
 import { useParams } from 'react-router-dom'
-
+import { useFetchDocument } from '../../hooks/useFetchDocument'
 
 const Post = () => {
 
     const { id } = useParams()
 
+    const { document: post, loading } = useFetchDocument("posts", id)
+
     return (
-        <div>
-            <h3>Post {id}</h3>
+        <div className={styles.post_container}>
+            {loading && <p>Loading...</p>}
+            {post && (
+                <>
+                    <h3>{post.title}</h3>
+                    <div className={styles.text}>
+                        <img src={post.image} alt={post.title} />
+                        <p>{post.body}</p>
+                    </div>
+                    <h4>The post is about</h4>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map((tag) => (
+                            <p key={tag}>
+                                <span>#</span>
+                                {tag}
+                            </p>
+                        ))}
+
+                    </div>
+                </>
+            )}
         </div>
     )
 }
