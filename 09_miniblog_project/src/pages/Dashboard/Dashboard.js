70de75cb5e60d1bfb3cd6c2f1031dcc75905afef
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-24 17:22:44 -0300

    Dashboard styling and functions

--- 09_miniblog_project/src/pages/Dashboard/Dashboard.js
+++ 09_miniblog_project/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,61 @@
 import styles from './Dashboard.module.css'
 
-const dashboard = () => {
+import { Link } from 'react-router-dom'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
+
+const Dashboard = () => {
+
+  const { user } = useAuthValue()
+  const uid = user.uid
+
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
+
+  const deleteDocument = (id) => {
+
+  }
+
+  if(loading){
+    return <p>Loading...</p>
+  }
+
   return (
-    <div>dashboard</div>
+    <div className={styles.dashboard}>
+      <h3>Manage your posts</h3>
+
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>No posts found</p>
+          <Link to="/posts/create" className='btn'>Create a post</Link>
+        </div>
+      ) : (
+        <>
+          <div className={styles.post_header}>
+            <span>Title</span>
+            <span>Actions</span>
+          </div>
+
+          {posts && posts.map((post) => (
+            <div key={post.id} className={styles.post_row}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">See</Link>
+
+                <Link to={`/post/edit/${post.id}`} className="btn btn-outline">Edit</Link>
+
+                <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Delete</button>
+              </div>
+              
+            </div> 
+          ))}
+
+        </>
+      )}
+
+    </div>
   )
 }
 
-export default dashboard
+export default Dashboard
