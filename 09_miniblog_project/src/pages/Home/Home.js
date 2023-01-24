c0036ce7f4c51182088be1a45600a656ea253881
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-23 21:35:39 -0300

    Home template and styling

--- 09_miniblog_project/src/pages/Home/Home.js
+++ 09_miniblog_project/src/pages/Home/Home.js
@@ -1,8 +1,40 @@
-import './Home.module.css'
+import styles from './Home.module.css'
+
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Home = () => {
+
+  const [query, setQuery] = useState("")
+
+  const [posts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.home}>
+      <h3>See our most recent posts</h3>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder='Or search for tags...'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className='btn btn-dark'>Search</button>
+      </form>
+
+      <div>
+        <h3>Posts...</h3>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>No posts found</p>
+            <Link to="/posts/create" className='btn'>Create a post</Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
