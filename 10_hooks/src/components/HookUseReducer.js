4a56b905f9999d85f675723a1034c86ac912f77e
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-27 14:21:36 -0300

    UseReducer

---
+++ 10_hooks/src/components/HookUseReducer.js
@@ -0,0 +1,18 @@
+import { useReducer } from "react"
+
+const HookUseReducer = () => {
+  const [number, dispatch] = useReducer((state, action) => {
+    return Math.random(state)
+  })
+
+  return (
+    <div>
+      <h2>useReducer</h2>
+      <p>Number: {number}</p>
+      <button onClick={dispatch}>Alter number</button>
+      <hr />
+    </div>
+  )
+}
+
+export default HookUseReducer
