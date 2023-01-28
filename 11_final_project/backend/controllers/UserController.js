bc30ad690aa8a9154726e114b162c964f33885ba
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-28 15:44:26 -0300

    Registering user in db

--- 11_final_project/backend/controllers/UserController.js
+++ 11_final_project/backend/controllers/UserController.js
@@ -5,12 +5,39 @@ const jwt = require("jsonwebtoken")
 const jwtSecret = process.env.JWT_PASS
 
 const generateToken = (id) => {
-    return jwt.sign({id, jwtSecret}, { expiresIn: "10d"})
+    return jwt.sign({ id }, jwtSecret, { expiresIn: "10d" })
 }
 
 // register user and sign in
-const register = async(req, res) => {
-    res.send("Register")
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+        res.status(422).json({ errors: ["Email already exists."] })
+        return
+    }
+
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    if (!newUser) {
+        res.status(422).json({ errors: ["Something went wrong. Try again later!"] })
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+
 }
 
 module.exports = {
