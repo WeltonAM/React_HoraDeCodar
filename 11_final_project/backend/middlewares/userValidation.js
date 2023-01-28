4677fa547e270737bae6f3656b7fe86f37f9058d
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-01-28 14:59:57 -0300

    Validate user register

---
+++ 11_final_project/backend/middlewares/userValidation.js
@@ -0,0 +1,34 @@
+const { body } = require("express-validator")
+
+const userCreateValidation = () => {
+    return [
+        body("name")
+            .isString()
+            .withMessage("The name is required.")
+            .isLength({ min: 3 })
+            .withMessage("The name needs to have at least 3 characters."),
+        body("email")
+            .isString()
+            .withMessage("The email is required.")
+            .isEmail()
+            .withMessage("Enter a valid email."),
+        body("password")
+            .isString()
+            .withMessage("The password is required.")
+            .isLength({ min: 6 })
+            .withMessage("The password needs to have 6 characters."),
+        body("confirmpassword")
+            .isString()
+            .withMessage("The password confirmation is required.")
+            .custom((value, {req}) => {
+                if(value != req.body.password){
+                    throw new Error("The password doesn't match.")
+                }
+                return true
+            })
+    ]
+}
+
+module.exports = {
+    userCreateValidation
+}
