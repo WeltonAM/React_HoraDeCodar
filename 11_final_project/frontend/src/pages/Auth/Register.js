16c018de516f3c0cf0226780c5522a1014678b55
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2023-02-02 18:17:36 -0300

    Redux

--- 11_final_project/frontend/src/pages/Auth/Register.js
+++ 11_final_project/frontend/src/pages/Auth/Register.js
@@ -4,9 +4,23 @@ import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 const Register = () => {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
   
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user);
   }
 
   return (
@@ -16,13 +30,33 @@ const Register = () => {
       <p className='subtitle'>Sign in to see your friends photos and share yours!</p>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Name' />
+        <input 
+          type="text" 
+          placeholder='Name' 
+          onChange={(e) => setName(e.target.value)} 
+          value={name} 
+        />
         
-        <input type="email" placeholder='Email' />
+        <input 
+          type="email" 
+          placeholder='Email'
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+        />
         
-        <input type="password" placeholder='Password'/>
+        <input 
+          type="password"  
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
         
-        <input type="password" placeholder='Confirm Password' />
+        <input 
+          type="password" 
+          placeholder='Confirm Password'
+          onChange={(e) => setConfirmPassword(e.target.value)} 
+          value={confirmPassword} 
+        />
         
         <input type="submit" value="Sign up" />
       </form>
